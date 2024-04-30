3e60a2ebae3d5d1c0f2d5b7d21cd6b826f3a815b
eyamrog <eyamrog@gmail.com>
2024-04-30 18:11:26 -0300

    Add files via upload

--- lmda.sh
+++ lmda.sh
@@ -11,8 +11,8 @@ rm -f tweets/tagged.txt  ### WATCH THIS!
 while read text conversation date user url 
 do
   echo "--- treetagging $text / $last ---"
-  rg $text tweets/emoji.txt | tr '|' '\n' | rg -v '^c:' > a
-  rg $text tweets/emoji.txt | cut -d'|' -f5 | sed 's/^c://' | tree-tagger-portuguese2 | gsed -e '/^@/s/\(.*\)	\(.*\)	\(.*\)/\1	\2	twitterhandle/' -e '/<unknown>$/s/\(.*\)	\(.*\)	\(.*\)/\1	\2	\1/' -e '/^EMOJI/s/\(EMOJI_\)\(.*\)	\(.*\)	\(EMOJI_\)\(.*\)/\2	EMOJI	\L\5/g' -e '/^HASHTAG/s/\(HASHTAG\)\(#\)\(.*\)	\(.*\)	\(HASHTAG\)\(#\)\(.*\)/\2\3	HASHTAG	\L\7/' -e 's/\(.*\)	\(.*\)	\(.*\)/\1	\2	\L\3/' | tr '\n' '~' | sed 's/^/c:/' >> a
+  rg $text tweets/tweets.txt | tr '|' '\n' | rg -v '^c:' > a
+  rg $text tweets/tweets.txt | cut -d'|' -f5 | sed 's/^c://' | tree-tagger-portuguese2 | gsed -e '/^@/s/\(.*\)	\(.*\)	\(.*\)/\1	\2	twitterhandle/' -e '/<unknown>$/s/\(.*\)	\(.*\)	\(.*\)/\1	\2	\1/' -e '/^EMOJI/s/\(EMOJI_\)\(.*\)	\(.*\)	\(EMOJI_\)\(.*\)/\2	EMOJI	\L\5/g' -e '/^HASHTAG/s/\(HASHTAG\)\(#\)\(.*\)	\(.*\)	\(HASHTAG\)\(#\)\(.*\)/\2\3	HASHTAG	\L\7/' -e 's/\(.*\)	\(.*\)	\(.*\)/\1	\2	\L\3/' | tr '\n' '~' | sed 's/^/c:/' >> a
   tr '\n' '|' < a | sed 's/~$//' >> tweets/tagged.txt
   echo >> tweets/tagged.txt 
 done < file_index.txt
