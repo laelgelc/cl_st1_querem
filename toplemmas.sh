e67cc6dd3a3e8d7774aa46d0a0c42325084ba4c4
eyamrog <eyamrog@gmail.com>
2024-05-01 15:50:53 -0300

    Add files via upload

--- toplemmas.sh
+++ toplemmas.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cut -d'|' -f2 tweets/types.txt | sed 's/^c://' | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' | grep '[a-z]' | sort | uniq -c | sort -nr | sed 's/^[ ]*//' | rg -v '^[123] ' | tr -dc '[:lower:][0-9][:punct]_\n ' | sed -f stoplist.sed > wordlist
+#cut -d'|' -f2 tweets/types.txt | sed 's/^c://' | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' | grep '[a-z]' | sort | uniq -c | sort -nr | sed 's/^[ ]*//' | rg -v '^[123] ' | tr -dc '[:lower:][0-9][:punct]_\n ' | sed -f stoplist.sed > wordlist
+
+# Note: The command " tr -dc '[:lower:][0-9][:punct]_\n ' " causes the removal of accented characters such as '[áàãéíóõúç]' and brings no apparent benefit to the analysis
+
+cut -d'|' -f2 tweets/types.txt | sed 's/^c://' | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' | grep '[a-z]' | sort | uniq -c | sort -nr | sed 's/^[ ]*//' | rg -v '^[123] ' | sed -f stoplist.sed > wordlist
 
 head -1000 wordlist | cut -d' ' -f2 | nl -nrz | tr '\t' ' ' | sed 's/^/v/' > selectedwords
 
