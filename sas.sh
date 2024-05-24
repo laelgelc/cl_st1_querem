41fdae48b53829691215fc4cda5793527e26482d
eyamrog <eyamrog@gmail.com>
2024-05-24 17:43:51 -0300

    Add files via upload

--- sas.sh
+++ sas.sh
@@ -4,11 +4,34 @@ mkdir -p sas
 
 rm -f columns
 
+
+# Considers the quantity of variables per text unitary even if they occur only once - suitable for short texts
 while read n word 
 do
   echo "--- $n ---"
-  rg -w $word tweets/types.txt | cut -d'|' -f1 | sed -e "s/$/ "$n" 1/" >> columns 
+  rg -w "$word" tweets/types.txt | cut -d'|' -f1 | sed -e "s/$/ "$n" 1/" >> columns 
 done < selectedwords
+
+# Considers the actual quantity of variables per text - suitable for longer texts
+# Initialize the output file
+#> columns
+
+# Read each line from the file "selectedwords"
+#while read n word; do
+#  echo "--- $n ---"
+#  
+#  # Process each line in "tweets/tokens.txt"
+#  while read line; do
+#    # Count occurrences of $word in the current line
+#    line_count=$(echo "$line" | grep -wo "$word" | wc -l)
+#    
+#    # Append the count to the output file if it's not zero
+#    if [ $line_count -gt 0 ]; then
+#      echo "$line" | cut -d'|' -f1 | sed -e "s/$/ $n $line_count/" >> columns
+#    fi
+#  done < tweets/tokens.txt
+#done < selectedwords
+
 
 sort columns | uniq > a ; mv a columns  # to avoid words whose accents were stripped to be duplicated in the same text ; SAS can't handle that
 
