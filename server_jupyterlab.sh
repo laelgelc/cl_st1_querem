1bdd4713e305a7aaba83cfabaa651133a5be929b
eyamrog <eyamrog@gmail.com>
2024-07-27 11:29:30 -0300

    Add files via upload

--- server_jupyterlab.sh
+++ server_jupyterlab.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-nohup jupyter-lab --ip 0.0.0.0 --no-browser --allow-root &
+source "$HOME"/my_env/bin/activate
+nohup jupyter-lab --ip 0.0.0.0 --no-browser --allow-root --ServerApp.root_dir="$HOME" &
 #jupyter server list
 
 echo "Update the 'GELCSG' inbound rule for port 8888 with the IP address of your computer"
-echo "Run 'https://<Public IPv4 DNS>:8888' on your browser and provide the server token on the file 'nohup.out'"
+echo "Run 'http://<Public IPv4 DNS>:8888' on your browser and provide the server token on the file 'nohup.out'"
